977dbbb66a261b4e6a88c7bf62bd637da92d668c
Hamza <144457566+unk-user@users.noreply.github.com>
2024-09-05 21:52:24 +0100

    add mobile navigation menu

--- src/components/layout/navbar/menu.tsx
+++ src/components/layout/navbar/menu.tsx
@@ -1,18 +1,25 @@
-import { Button } from '@/components/ui/button';
 import Link from 'next/link';
+
+import { Button } from '@/components/ui/button';
 import { CollectionMenu } from './collection-menu';
 import { getNavbarCollections } from '@/lib/shopify';
+import { MobileNavmenu } from './mobile-menu';
 
+//TODO: HANDLE DATA BOUNDERIES BETTER
 export async function NavMenu() {
-  const navbarCollections = await getNavbarCollections()
+  const collections = await getNavbarCollections();
+  if (!collections) throw new Error('No collections');
 
   return (
     <nav>
-      <ul className="space-x-1 flex items-center *:py-2">
+      <ul className="space-x-1 hidden md:flex items-center *:py-2">
         <ListItem href="/products" label="Shop" />
-        <CollectionMenu collections={navbarCollections} />
+        <CollectionMenu collections={collections} />
         <ListItem href="/about" label="About" />
       </ul>
+      <div className="md:hidden">
+        <MobileNavmenu collections={collections}/>
+      </div>
     </nav>
   );
 }
---
+++ src/components/layout/navbar/mobile-menu.tsx
@@ -0,0 +1,161 @@
+'use client';
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { useEffect, useState } from 'react';
+import { useMediaQuery } from 'react-responsive';
+import { ChevronLeft, ChevronRight, Menu } from 'react-feather';
+import { NavbarCollection } from '@/lib/shopify/types';
+import { Button } from '@/components/ui/button';
+import { motion, Variants } from 'framer-motion';
+
+import Link from 'next/link';
+
+const mainVariants: Variants = {
+  open: {
+    x: 'calc(-50% - 0.75rem)',
+    transition: {
+      ease: 'easeInOut',
+      duration: 0.2,
+      type: 'tween',
+    },
+  },
+  closed: {
+    x: 0,
+    transition: {
+      ease: 'easeInOut',
+      duration: 0.2,
+      type: 'tween',
+    },
+  },
+};
+
+//TODO: REFACTOR COMPONENTS
+
+export function MobileNavmenu({
+  collections,
+}: {
+  collections: NavbarCollection;
+}) {
+  const [open, setOpen] = useState(false);
+  const [slideOut, setSlideOut] = useState(false);
+  const isMobile = useMediaQuery({ query: '(max-width: 768px)' });
+
+  const handleOpen = () => {
+    if (!open) {
+      setSlideOut(false);
+      setOpen(true);
+    } else {
+      setOpen(false);
+    }
+  };
+
+  useEffect(() => {
+    if (!isMobile) setOpen(false);
+  }, [isMobile]);
+
+  return (
+    <Sheet open={open} onOpenChange={handleOpen}>
+      <div className="h-full py-2">
+        <Button size="icon" variant="icon" asChild>
+          <SheetTrigger className="flex items-center">
+            <Menu
+              height={24}
+              width={24}
+              strokeWidth={1.5}
+              className="text-primary-foreground"
+            />
+          </SheetTrigger>
+        </Button>
+      </div>
+      <SheetContent side="left" className="pt-4 overflow-hidden">
+        <SheetHeader className="hidden">
+          <SheetTitle>Navbar Menu</SheetTitle>
+          <SheetDescription>Menu Links</SheetDescription>
+        </SheetHeader>
+        <motion.div
+          variants={mainVariants}
+          animate={slideOut ? 'open' : 'closed'}
+          className="flex w-[calc(200%+1.5rem)] gap-6"
+        >
+          <div title="Main" className="w-full">
+            <ul className="w-full mt-8">
+              <li>
+                <ListLink label="Shop" href="/products" onClick={handleOpen} />
+              </li>
+              <li>
+                <Button
+                  variant="mobileNavlink"
+                  size="custom"
+                  onClick={() => setSlideOut(true)}
+                >
+                  Collections
+                  <ChevronRight width={24} height={24} className="ml-auto" />
+                </Button>
+              </li>
+              <li>
+                <ListLink label="About" href="/about" onClick={handleOpen} />
+              </li>
+            </ul>
+          </div>
+          <div title="Collections" className="w-full h-full relative pt-8">
+            <Button
+              variant="ghost"
+              className="absolute -top-2 h-8 w-full hover:bg-transparent justify-start px-1 gap-1s"
+              onClick={() => setSlideOut(false)}
+            >
+              <ChevronLeft width={16} height={16} />
+              All
+            </Button>
+            <ListLink
+              label="Collections"
+              href="/products"
+              onClick={handleOpen}
+            />
+            <ul className="-space-y-1">
+              {collections.map((collection) => (
+                <li key={collection.handle}>
+                  <ListLink
+                    label={collection.title}
+                    href={`/products/${collection.handle}`}
+                    className="font-normal text-base"
+                    onClick={handleOpen}
+                  />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </motion.div>
+      </SheetContent>
+    </Sheet>
+  );
+}
+
+const ListLink = ({
+  label,
+  href,
+  className,
+  onClick,
+}: {
+  label: string;
+  href: string;
+  className?: string;
+  onClick?: () => void;
+}) => {
+  return (
+    <Button
+      variant="mobileNavlink"
+      size="custom"
+      className={className}
+      onClick={onClick}
+      asChild
+    >
+      <Link href={href}>{label}</Link>
+    </Button>
+  );
+};
