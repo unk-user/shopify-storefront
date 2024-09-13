f1ba5926980ea6867f25aae2c49f8924f9584d4b
Hamza <144457566+unk-user@users.noreply.github.com>
2024-09-13 20:11:51 +0100

    feat: fix revalidation when user navigates

--- src/components/cart/actions.ts
+++ src/components/cart/actions.ts
@@ -7,7 +7,7 @@ import {
   removeFromCart,
   updateCart,
 } from '@/lib/shopify';
-import { revalidateTag } from 'next/cache';
+import { revalidatePath, revalidateTag } from 'next/cache';
 import { cookies } from 'next/headers';
 import { redirect } from 'next/navigation';
 
@@ -23,63 +23,53 @@ export async function createCartAndSetCookie() {
 }
 
 export async function addItem(
-  prevState: any,
-  {
-    selectedVariantId,
-    quantity = 1,
-  }: { selectedVariantId?: string; quantity?: number }
+  selectedVariantId: string,
+  quantity: number = 1
 ): Promise<ActionResponse> {
   let cartId =
     cookies().get('cartId')?.value || (await createCartAndSetCookie());
 
-  if (!cartId) return { status: 'error', message: 'Cart ID not found' };
-  if (!selectedVariantId)
-    return { status: 'error', message: 'Variant not selected' };
-
   try {
-    const { userErrors } = await addToCart(cartId, [
-      { merchandiseId: selectedVariantId, quantity },
-    ]);
+    revalidateTag(TAGS.cart);
 
+    if (!cartId) return { status: 'error', message: 'Cart ID not found' };
+    if (!selectedVariantId)
+      return { status: 'error', message: 'Variant not selected' };
+
+    await addToCart(cartId, [{ merchandiseId: selectedVariantId, quantity }]);
     return {
       status: 'success',
       message: 'Cart action confirmed',
     };
   } catch (e) {
-    console.log(e);
     return { status: 'error', message: 'Error adding item to cart' };
-  } finally {
-    revalidateTag(TAGS.cart);
   }
 }
 
-export async function removeItem(prevState: any, merchandiseId: string) {
-  let cartId = cookies().get('cartId')?.value;
-
-  if (!cartId) {
-    return 'Missing cart ID';
-  }
-
+export async function removeItem(
+  merchandiseId: string
+): Promise<ActionResponse> {
   try {
-    const cart = await getCart(cartId);
+    revalidateTag(TAGS.cart);
 
-    if (!cart) {
-      return 'Error fetching cart';
-    }
+    let cartId = cookies().get('cartId')?.value;
+    if (!cartId) return { status: 'error', message: 'Missing cart ID' };
+
+    const cart = await getCart(cartId);
+    if (!cart) return { status: 'error', message: 'Error fetching cart' };
 
     const lineItem = cart.lines.find(
       (line) => line.merchandise.id === merchandiseId
     );
+    if (!lineItem || !lineItem.id)
+      return { status: 'error', message: 'Error removing item from cart' };
 
-    if (lineItem && lineItem.id) {
-      await removeFromCart(cartId, [lineItem.id]);
-      revalidateTag(TAGS.cart);
-    } else {
-      return 'Item not found in cart';
-    }
+    await removeFromCart(cartId, [lineItem.id]);
+    return { status: 'success', message: 'Cart action confirmed' };
   } catch (e) {
+    return { status: 'error', message: 'Error removing item from cart' };
+  } finally {
     revalidateTag(TAGS.cart);
-    return 'Error removing item from cart';
   }
 }
 
