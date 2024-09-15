87af6414390502412723767491c5c7cca41fdc64
Hamza <144457566+unk-user@users.noreply.github.com>
2024-09-15 20:45:33 +0100

    fix text align

--- src/components/Prose.tsx
+++ src/components/Prose.tsx
@@ -10,7 +10,7 @@ const Prose: FunctionComponent<TextProps> = ({ html, className }) => {
   return (
     <div
       className={cn(
-        'prose max-w-none w-full prose-headings:text-primary-foreground prose-img:w-full prose-img:!mx-0 prose-headings:!mb-2 prose-headings:lg:!text-center *:max-lg:!text-justify prose-p:!m-0 prose-p:lg:!text-center prose-img:mt-24 *:!text-lg prose-h1:!text-3xl',
+        'prose max-w-none w-full prose-headings:text-primary-foreground prose-img:w-full prose-img:!mx-0 prose-headings:!mb-2 prose-headings:lg:!text-center *:max-lg:!text-start prose-p:!m-0 prose-p:lg:!text-center prose-img:mt-24 *:!text-lg prose-h1:!text-3xl',
         className
       )}
       dangerouslySetInnerHTML={{ __html: html }}
