738d9d4162320673ed71b58fba6370e1263bd8a1
Hamza <144457566+unk-user@users.noreply.github.com>
2024-09-17 11:33:06 +0100

    feat: landing page hero section

---
+++ src/components/layout/landing-page/hero-section.tsx
@@ -0,0 +1,35 @@
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+import { ArrowUpRight } from 'react-feather';
+
+export function HeroSection() {
+  return (
+    <section className="section-default py-16 max-md:px-4">
+      <div className="mx-auto lg:text-center *:lg:mx-auto">
+        <h1
+          className="text-4xl font-semibold max-w-4xl bg-gradient-to-r from-storefront-primary-300 to-primary-foreground
+            bg-clip-text text-transparent max-md:text-3xl"
+        >
+          Elevate Every Keystroke with Our Professional Mechanical Keyboards
+        </h1>
+        <p
+          title="Subheader"
+          className="max-w-3xl max-md:text-base mt-4 max-md:mt-2"
+        >
+          Discover the perfect blend of performance, precision, and aesthetics
+          with Keychron&apos;s versatile mechanical keyboards.
+        </p>
+        <Button
+          variant="primary"
+          className="px-14 mt-6 uppercase group"
+          asChild
+        >
+          <Link href="/products">
+            Shop now
+            <ArrowUpRight className="ml-2 group-hover:rotate-45 group-active:rotate-45 transition-transform duration-150" />
+          </Link>
+        </Button>
+      </div>
+    </section>
+  );
+}
