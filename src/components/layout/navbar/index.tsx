c4f81d63bc2f5c1493858aa6cc844012dec8ea4a
Hamza <144457566+unk-user@users.noreply.github.com>
2024-10-28 17:24:50 +0100

    remove search button

--- src/components/layout/navbar/index.tsx
+++ src/components/layout/navbar/index.tsx
@@ -1,6 +1,5 @@
 import { NavbarWrapper } from './navbar-wrapper';
 import { NavMenu } from './menu';
-import { SearchButton } from './search-button';
 import { CartButton } from './cart-button';
 
 export function Navbar() {
@@ -8,7 +7,6 @@ export function Navbar() {
     <NavbarWrapper>
       <NavMenu />
       <div className="ml-auto space-x-1 py-2">
-        <SearchButton />
         <CartButton />
       </div>
     </NavbarWrapper>
